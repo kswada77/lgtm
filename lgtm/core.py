d9bb9d4996653e9f9291d093652e43cad8293ca0
unknown <kswada77@gmail.com>
2021-02-06 18:45:49 +0900

    'gitignoreを追加'

---
+++ lgtm/core.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+@click.command()
+def cli():
+	"""LGTM画像生成ツール"""
+	lgtm()
+	click.echo('lgtm')
+	
+
+def lgtm():
+	pass
+	
---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from lgtm import core
+
+if __name__ == '__main__':
+	core.cli()
